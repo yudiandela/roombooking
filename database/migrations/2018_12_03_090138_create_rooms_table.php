9e5c1327a6ecd29e6d04fc5c32367c5c72b257bf
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-03 19:16:14 +0700

    change migration

--- database/migrations/2018_12_03_090138_create_rooms_table.php
+++ database/migrations/2018_12_03_090138_create_rooms_table.php
@@ -16,6 +16,7 @@ class CreateRoomsTable extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('area_id');
+            $table->unsignedInteger('facility_id');
             $table->string('name', 255);
             $table->text('description');
             $table->integer('capacity');
@@ -26,6 +27,7 @@ class CreateRoomsTable extends Migration
             $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
 
+            $table->foreign('facility_id')->references('id')->on('facilities')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade')->onUpdate('cascade');
         });
     }
